48da0508082704fbd09d9deb5a8ec9d4a6b1f9d1
Giacomo <giacomo.folli@studenti.unipr.it>
2025-02-21 19:30:02 +0100

    top

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,6 @@ import java.util.zip.InflaterInputStream;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
     System.err.println("Logs from your program will appear here!");
 
     final String command = args[0];
@@ -25,7 +23,8 @@ public class Main {
           head.createNewFile();
           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
           System.out.println("Initialized git directory");
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
           throw new RuntimeException(e);
         }
       }
@@ -38,10 +37,12 @@ public class Main {
         File blobFile = new File("./.git/objects/" + dir + "/" + file);
         try {
           String blob = new BufferedReader(
-              new InputStreamReader(new InflaterInputStream(new FileInputStream(blobFile)))).readLine();
+              new InputStreamReader(new InflaterInputStream(new FileInputStream(blobFile))))
+                  .readLine();
           String content = blob.substring(blob.indexOf('\0') + 1);
           System.out.print(content);
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
           throw new RuntimeException(e);
         }
       }
