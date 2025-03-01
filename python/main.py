c6429b7b178dece1369437c8be9df9774a47e714
Giacomo <giacomo.folli@studenti.unipr.it>
2025-03-01 10:31:07 +0100

    sbam

--- python/main.py
+++ python/main.py
@@ -1,53 +1,87 @@
 import argparse
 import hashlib
 import os
-import sys
 import zlib
 
 
 class Cli:
-    __commands = ["init", "cat-file", "hash-object", "ls-tree"]
+    main_dir = ".sbam"
+    objects_dir = os.path.join(main_dir, "objects")
+    refs_dir = os.path.join(main_dir, "refs")
+    head_file = os.path.join(main_dir, "HEAD")
 
-    def __init__(self, args):
-        self.__args = args
-        self.__commands_handler = {}
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description="sbam: a simple git-like version control system"
+        )
+        subparsers = self.parser.add_subparsers(dest="command", required=True)
 
-        self.__commands_handler["init"] = self.init
-        self.__commands_handler["cat-file"] = self.cat_file
-        self.__commands_handler["ls-tree"] = self.ls_tree
-        self.__commands_handler["hash-object"] = self.hash_object
+        # init command
+        init_parser = subparsers.add_parser(
+            "init", help="Initialize a new sbam repository"
+        )
+        init_parser.add_argument(
+            "-c",
+            "--crazy",
+            action="store_true",
+            help="Additional flag for init command",
+        )
+        init_parser.set_defaults(func=self.init)
+
+        # cat-file command
+        cat_file_parser = subparsers.add_parser(
+            "cat-file", help="Print the contents of a specified file"
+        )
+        cat_file_parser.add_argument("hash", help="The hash of the file to print")
+        cat_file_parser.set_defaults(func=self.cat_file)
+
+        # hash-object command
+        hash_object_parser = subparsers.add_parser(
+            "hash-object", help="Hash the specified file"
+        )
+        hash_object_parser.add_argument("file", help="The file to hash")
+        hash_object_parser.set_defaults(func=self.hash_object)
+
+        # ls-tree command
+        ls_tree_parser = subparsers.add_parser(
+            "ls-tree", help="List the contents of a tree object"
+        )
+        ls_tree_parser.set_defaults(func=self.ls_tree)
 
     def mkdir_if_not_exists(self, path):
         if not os.path.exists(path):
             os.mkdir(path)
 
-    def git_already_initialized(self) -> bool:
-        return os.path.exists(".git")
+    def sbam_already_initialized(self) -> bool:
+        return os.path.exists(self.main_dir)
 
-    def init(self):
-        self.mkdir_if_not_exists(".git")
-        self.mkdir_if_not_exists(".git/objects")
-        self.mkdir_if_not_exists(".git/refs")
+    def init(self, args):
+        self.mkdir_if_not_exists(self.main_dir)
+        self.mkdir_if_not_exists(self.objects_dir)
+        self.mkdir_if_not_exists(self.refs_dir)
 
-        with open(os.path.join(".git", "HEAD"), "w") as temp_file:
+        with open(self.head_file, "w") as temp_file:
             temp_file.write("ref: refs/heads/main\n")
 
-        print("Initialized git directory")
+        if args.crazy:
+            print("What are you flag is set!")
 
-    def cat_file(self):
-        hash = self.__args[2]
+        print("Initialized sbam directory")
+
+    def cat_file(self, args):
+        hash = args.hash
         dir = hash[:2]
         file = hash[2:]
 
-        with open(os.path.join(".git", "objects", dir, file), "rb") as file:
+        with open(os.path.join(self.objects_dir, dir, file), "rb") as file:
             decomp = zlib.decompress(file.read())
 
         start = decomp.find(b"\x00")
         parsed = decomp[start + 1 :].strip(b"\n").decode("utf-8")
         print(parsed)
 
-    def hash_object(self):
-        fName = self.__args[2]
+    def hash_object(self, args):
+        fName = args.file
 
         with open(fName, "rb") as file:
             bytes = file.read()
@@ -56,8 +90,8 @@ class Cli:
         store = header.encode("ascii") + bytes
         hash = hashlib.sha1(store).hexdigest()
 
-        if self.git_already_initialized():
-            new_dir = os.path.join(os.getcwd(), ".git/objects", hash[:2])
+        if self.sbam_already_initialized():
+            new_dir = os.path.join(os.getcwd(), self.objects_dir, hash[:2])
 
             self.mkdir_if_not_exists(new_dir)
             with open(os.path.join(new_dir, hash[2:]), "wb") as file:
@@ -65,52 +99,14 @@ class Cli:
 
         print(hash)
 
-    def ls_tree(self):
+    def ls_tree(self, args):
         print("To implement")
 
-    def instructions(self):
-        print(
-            """
-    Wrong syntax in call. Expected syntax is: git [action]
-
-    Supported actions are:
-    - init: Initialize new git repository
-    - cat-file: print to console contents of specified file
-    - hash-objects: hash the specified file"""
-        )
-
     def run(self):
-        print("DEBUG: args ->", self.__args)
-
-        parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            "command",
-            choices=[cmd for cmd in self.__commands],
-            help="Command to execute",
-        )
-
-        parser.add_argument(
-            "-c",
-            "--crazy",
-            action="store_true",
-            help="Additional flag for init command",
-        )
-        parser.add_argument("object", help="Object to hash")
-
-        args = parser.parse_args()
-
-        # TODO: Update to better handling of argparse
-
-        command = args.command
-
-        if command in self.__commands_handler.keys():
-            handle_func = self.__commands_handler[command]
-            handle_func()
-        else:
-            print("Unknown command: " + command)
+        args = self.parser.parse_args()
+        args.func(args)
 
 
 if __name__ == "__main__":
-    cli = Cli(sys.argv)
+    cli = Cli()
     cli.run()
